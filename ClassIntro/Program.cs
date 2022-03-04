46489603c424392d3c716129d300548f788c43cc
Atakan Arslan <1030520985@erciyes.edu.tr>
2022-03-04 15:05:38 +0300

    4.03.2022    15:05

--- ClassIntro/Program.cs
+++ ClassIntro/Program.cs
@@ -9,10 +9,13 @@ namespace ClassIntro
             string adi = "Engin";
             int yas = 36;
 
-            Kurs kurs1=new Kurs();
+
+
+            Kurs kurs1 = new Kurs();
             kurs1.KursAdi = "C#";
             kurs1.Egitmen = "Engin Demiroğ";
             kurs1.IzlenmeOrani = 68;
+
             //Bunlar kendimizin tanımlamış olduğu birer değişkendirler. 
             Kurs kurs2 = new Kurs();
             kurs2.KursAdi = "Java";
